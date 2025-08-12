cd4998719ddcac89cbb8f6ed83a7a007eabfbbd6
Pravin Malge <pravinmalge123@gmail.com>
2025-08-12 19:25:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shop in Candy Store/shop-in-candy-store.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    vector<int> minMaxCandy(vector<int>& prices, int k) {
+        // Code here
+        int n = prices.size();
+        sort(prices.begin(), prices.end());
+        int i = 0, j = n - 1;
+        int maxSum = 0;
+        int minSum = 0;
+        
+        while(i<=j){
+            minSum += prices[i];
+            j -= k;
+            i++;
+        }
+        
+        i=0;
+        j=n-1;
+        
+        while(i<=j){
+            maxSum += prices[j];
+            i += k;
+            j--;
+        }
+        
+        return {minSum, maxSum};
+    }
+};
